30be87c18ce0b6e9222e15b844336ef03d5219e1
quantummole <35193295+quantummole@users.noreply.github.com>
2020-05-11 23:37:39 +0530

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -15,29 +15,59 @@ class NewtonRaphson :
     def search(self,x_0,max_epochs) :
         epochs = max_epochs
         f_0 = self.func(x_0)
+        xs = [x_0]
+        fs = [f_0]
         while epochs > 0 and np.abs(f_0)>self.eps :
             f1_0 = self.derivative(x_0)
             x_0 = x_0 - f_0/f1_0
             f_0 = self.func(x_0)
-        return x_0,f_0
+            epochs = epochs - 1
+            xs.append(x_0)
+            fs.append(f_0)
+        return x_0,f_0,xs,fs
     
 if __name__ == '__main__' :
-    func = lambda x : x**4-x**2-2
-    derivative = lambda x : 4*x**3 - 2*x
+    func = lambda x :np.cos(3*x) - x
+
+    xs = np.linspace(-2,2,100)
+    ys = [func(x) for x in xs]
+    plt.plot(xs,ys)
+    plt.show()
+    
+    derivative = lambda x : -3*np.sin(3*x) -1
     solver = NewtonRaphson(func,derivative)
-    x,f = solver.search(3.1,100)
+    x,f,_,_ = solver.search(-0.75,100)
     print("Actual derivative ",x,f)
 
     delta = 1e-4
     derivative = lambda x : (func(x+delta)-func(x))/delta
     solver = NewtonRaphson(func,derivative)
-    x,f = solver.search(3.1,100)
+    x,f,x0s,fs = solver.search(-0.75,100)
     print("Derivative using finite difference ",x,f)
 
 
     xs = np.linspace(x-1,x+1,100)
     ys = [func(x) for x in xs]
     plt.plot(xs,ys)
+    plt.plot(x0s,fs)
     plt.show()
     
+    func = lambda x :np.cos(3*x) - x
+    derivative = lambda x : -3*np.sin(3*x) -1
+    solver = NewtonRaphson(func,derivative)
+    x,f,_,_ = solver.search(0.75,100)
+    print("Actual derivative ",x,f)
+
+    delta = 1e-4
+    derivative = lambda x : (func(x+delta)-func(x))/delta
+    solver = NewtonRaphson(func,derivative)
+    x,f,x0s,fs = solver.search(0.75,100)
+    print("Derivative using finite difference ",x,f)
+
+
+    xs = np.linspace(x-1,x+1,100)
+    ys = [func(x) for x in xs]
+    plt.plot(xs,ys)
+    plt.plot(x0s,fs)
+    plt.show()
     
